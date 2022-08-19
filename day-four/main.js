4427bc2b6569e49fe982e21f7793ed691ac4cc22
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2022-08-19 08:45:24 -0300

    Create main.js

---
+++ day-four/main.js
@@ -0,0 +1,13 @@
+const calculator = document.querySelector('.calculator')
+
+const keys = calculator.querySelector('.calculator__keyboard')
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')){
+
+  }
+})
+
+const {
+  
+}
