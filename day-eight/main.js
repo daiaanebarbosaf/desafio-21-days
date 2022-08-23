ca793a6447ddba07a3c1789b22518bb7158e4ff1
Daiane Farias <daiaanebarbosaf@gmail.com>
2022-08-23 16:31:54 -0300

    Update main.js

--- day-eight/main.js
+++ day-eight/main.js
@@ -12,13 +12,15 @@ const initialColors = {
     bgPanel: getStyle(html, "--bg-panel"),
     colorHeadings: getStyle(html, "--color-headings"),
     colorText: getStyle(html, "--color-text"),
+    ul: getStyle(html, "--ul-color"),
 }
 
 const darkMode = {
     bg: "#e0bcdd",
     bgPanel: "#4C3575",
     colorHeadings: "#4C3575",
-    colorText: "#4C3575"
+    colorText: "#4C3575",
+    ul:"fff0f6"
 }
 
 const transformKey = key => 
