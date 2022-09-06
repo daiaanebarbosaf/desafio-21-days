ee5e780a46ee0d61c09e29cb7ede9c26ba01241d
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2022-09-06 12:01:14 -0300

    Update main.js

--- day-twenty-one/main.js
+++ day-twenty-one/main.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', onScroll)
 const navigation = document.getElementById('navigation')
 
 onScroll()
-//Gerencia os Scrolls da página
+
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
@@ -16,39 +16,25 @@ function onScroll() {
 
 function activateMenuAtCurrentSection(section) {
     const targetLine = scrollY + innerHeight / 2
-    
-    //verificar se a seção passou da linha
-    //quais dados vou precisar?
-
-    //offsetTop nos trás a informação de onde se inicia a página.
+ 
     const sectionTop = section.offsetTop
-    //offsetTop nos trás a informação da altura da página.
+
     const sectionHeight = section.offsetHeight
 
-    //o topo da seção chegou ou ultrapassou a linha alvo?
+
     const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
 
-    //informações dos dados da lógica
-    //console.log(
-     //       'O topo da seção chegou ou passou da linha?',
-    //        sectionTopReachOrPassedTargetLine
-    //)
-
-    //verificar se a base está abaixo da linha alvo
-    //quais dados vou precisar?
     
-    //a seção termina onde?
     const sectionEadAt = sectionTop + sectionHeight
 
-    //o final da seção passou da linha do alvo
+
     const sectionEndPassedTargetline = sectionEadAt <= targetLine
 
-    //console.log(
-    //    'O fundo da seção passou da linha alvo',
-        sectionEndPassedTargetline
-   // )
 
-    //limites da seção
+        sectionEndPassedTargetline
+
+
+ 
     const sectionBoundaries =
     sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetline
     
